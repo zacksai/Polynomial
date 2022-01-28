8b05d0968d3cdf2a7ca3e94ff5a035b3cd56d670
zacksai <zackjsai@gmail.com>
2022-01-27 20:57:04 -0800

    Updated coefficient & exponent storage rules and some comments

--- src/Term.java
+++ src/Term.java
@@ -1,38 +1,38 @@
 /**
  * Term class contains a term of a polynomial. Examples: x^4, a^32, -10b^2, +3z
- *
+ * <p>
  * A term has up to 5 parts:
- *      Sign:           + || -
- *      Coefficient:    ints only for simplicity
- *      Term:           [a-z] || [A-Z]
- *      Power carat:    ^
- *      Exponent:       ints only for simplicity
- *
+ * Sign:           + || -
+ * Coefficient:    ints only for simplicity
+ * Term:           [a-z] || [A-Z]
+ * Power carat:    ^
+ * Exponent:       ints only for simplicity
+ * <p>
  * Delimiters:
- *              ^: separates term from exponent
- *                  lack of ^ indicates no exponent
- *
- *
+ * ^: separates term from exponent
+ * lack of ^ indicates no exponent
+ * the base of the term itself, which is the only non-number character outside of the ^
  */
 public class Term {
 
     // PROPERTIES:
-    public final String LEGAL_LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ";
-    public final String LEGAL_NUMBERS = "1234567890";
-    public final String LEGAL_SIGNS = "+-";
-    private int coefficient;
-    private int exponent;
+    private int coefficient = 1;
+    private int exponent = 1;
     private char term;
 
 
+    /**
+     * empty constructor initializes term object (variables are already default)
+     */
     public Term() {
     }
 
     /**
-     * 3-param constructor initializes term given a coefficient, exponent, and term)
+     * 3-param constructor initializes term given a coefficient, exponent, and term
+     *
      * @param coefficient the coefficient of the term
-     * @param exponent the exponent of the term
-     * @param term the alphabetical term itself
+     * @param exponent    the exponent of the term
+     * @param term        the alphabetical term itself
      */
     public Term(int coefficient, int exponent, char term) {
         this.coefficient = coefficient;
@@ -40,6 +40,7 @@ public class Term {
         this.term = term;
     }
 
+    // Accessors and modifiers
     public int getCoefficient() {
         return coefficient;
     }
@@ -64,10 +65,32 @@ public class Term {
         this.term = term;
     }
 
+    /**
+     * toString returns a properly formatted string for the polynomial term
+     *
+     * @return formatted polynomial term
+     */
     @Override
     public String toString() {
-        String termString = "";
-        if (term != '\u0000') termString = String.valueOf(term);
-        return coefficient + termString + "^" + exponent;
+
+        // Convert coefficient to string
+        String coefficientString = String.valueOf(coefficient);
+
+        // Handle empty terms first:
+        if (term == '\u0000') return coefficientString;
+
+
+        // Next, handle terms with coefficient 1 or -1:
+        if (coefficient == 1) coefficientString = "";
+        if (coefficient == -1) coefficientString = "-";
+
+        // Convert the term to string
+        String termString = String.valueOf(term);
+
+        // Next, handle exponents and begin return statements
+        if (exponent == 0 || exponent == 1) return coefficientString + termString;
+
+        // Finally, return full concatenation
+        return coefficientString + termString + "^" + exponent;
     }
 }
--- src/TermParser.java
+++ src/TermParser.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
  * The first thing we're going to want to do is read a string
  * We can do that using the console and a scanner.
  * Once we have the string, we want to separate it into its parts
- * <p>
  * A term has up to 5 parts:
  * Sign:           + || -
  * Coefficient:    ints only for simplicity
@@ -30,9 +29,9 @@ import java.util.Scanner;
  * -   2
  * -           x
  * -   2       x
- * x       ^   3
+ *             x       ^   3
  * -           x       ^   3
- * 2       x       ^   3
+ *     2       x       ^   3
  * -   2       x       ^   3
  * <p>
  * Clearly the best place to start is the exponent. The carat is unique value, making it a good delimiter
@@ -40,7 +39,7 @@ import java.util.Scanner;
  * If the carat does not exist, continue by working back to front with the remaining term:
  * <p>
  * Once we've removed the exponent, the remaining string can only have terms and coefficients
- * We can separate the rest of the string into pieces by working back to front:
+ * We can separate the rest of the string into pieces by working :back to front
  * Check the last char. If it is NOT [a-z] || [A-Z], then it must be a number, so store coefficient. all done!
  * If the last char IS a term, store the term, and continue to work back to front.
  * Now, the last char can only be a sign or a number
@@ -48,10 +47,6 @@ import java.util.Scanner;
  * If it's a number, store the whole rest of the string as the coefficient. all done!
  */
 public class TermParser {
-
-    public final String LEGAL_LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ";
-    public final String LEGAL_NUMBERS = "1234567890";
-    public final String LEGAL_SIGNS = "+-";
 
     // Step 1: Check for exponent
     // If exponent, store first
@@ -76,7 +71,8 @@ public class TermParser {
         // Create temporary term to store values
         Term tempTerm = new Term();
 
-        // Check for exponent
+
+        // Check for exponent (handle case that only a carat is passed)
         if (hasExponent(inputTerm)) {
 
             // If it has an exponent, store it in tempTerm (convert to integer and use substring after carat)
@@ -84,8 +80,8 @@ public class TermParser {
 
             // Delete what remains after the carat
             inputTerm = inputTerm.substring(0, inputTerm.indexOf("^"));
+
         }
-        System.out.println("Check for exponent" + tempTerm.toString());
 
 
         // Next, check if it's just a coefficient remaining
@@ -94,35 +90,52 @@ public class TermParser {
             // Store coefficient in tempTerm
             tempTerm.setCoefficient(Integer.parseInt(inputTerm));
         } else {
-            System.out.println("is not coefficient" + inputTerm);
 
-            // If it isn't, last character is the term, so store it
-            char term = inputTerm.charAt(inputTerm.length()-1);
-            tempTerm.setTerm(term);
+            // If it isn't, last character is the term, so check if it's legal then store it
+            char term = inputTerm.charAt(inputTerm.length() - 1);
+            if (isValidChar(term)) tempTerm.setTerm(term);
 
             // Trim out the term character
-            inputTerm = inputTerm.substring(0, inputTerm.length()-1);
-
-            System.out.println("term stored" + tempTerm);
-
+            inputTerm = inputTerm.substring(0, inputTerm.length() - 1);
 
             // Next, check if the remaining term is a coefficient or just a "+" or "-":
             if (isSign(inputTerm)) {
 
                 // If it's just a sign with no number, set the coefficient appropriately
-                if (inputTerm.charAt(inputTerm.length()) == '-') tempTerm.setCoefficient(-1);
-                else tempTerm.setCoefficient(1);
+                if (inputTerm.charAt(inputTerm.length() - 1) == '-') tempTerm.setCoefficient(-1);
 
                 // Otherwise, the remaining term is the coefficient
             } else if (inputTerm.length() > 0) tempTerm.setCoefficient(Integer.parseInt(inputTerm));
 
         }
 
+        // At this point, the term is totally initialized, but we still have to handle certain formatting issues:
+
+
+        // Handle case: coefficient^exponent (no term, ie 2^5 or 3^2):
+        if (tempTerm.getTerm() == '\u0000') {
+            double value = Math.pow(Double.valueOf(tempTerm.getCoefficient()), Double.valueOf(tempTerm.getExponent()));
+            tempTerm.setCoefficient((int) value);
+            tempTerm.setExponent(1);
+        }
+
+        // Handle case: term^0 (ie -4x^0 or z^0)
+        if (tempTerm.getExponent() == 0) {
+
+            // Remove term, leaving only coefficient. Redundancy in case anything slips through previous case
+            if (tempTerm.getTerm() != '\u0000') {
+                tempTerm.setTerm('\u0000');
+            }
+        }
+
+        // Handle case: coefficient = 0
+        if (tempTerm.getCoefficient() == 0) {
+            tempTerm.setTerm('\u0000');
+            tempTerm.setExponent(1);
+        }
 
         // Test statement
-        System.out.println(tempTerm.toString());
-
-
+        System.out.println(tempTerm);
     }
 
     /**
@@ -132,29 +145,69 @@ public class TermParser {
      * @return true if exponent exists, false otherwise
      */
     private static boolean hasExponent(String termToCheck) {
-        System.out.println("hasexponent test");
         if (termToCheck.contains("^")) return true;
         return false;
     }
 
+    /**
+     * check if the remainder of a term is just a coefficient (no x or z or other term)
+     * @param termToCheck the term being checked
+     * @return true if the last character is 0-9
+     */
     private static boolean isJustCoefficient(String termToCheck) {
-        System.out.println("isJustCoefficient test");
-        Term tempTerm = new Term();
-        if (tempTerm.LEGAL_NUMBERS.contains(String.valueOf(termToCheck.charAt(termToCheck.length()-1)))) return true;
+
+        // Make a list of legal numbers and return if termToCheck ends in one or not
+        String legalNumbers = "0123456789";
+        if (legalNumbers.contains(String.valueOf(termToCheck.charAt(termToCheck.length() - 1)))) return true;
         return false;
     }
 
+    /**
+     * a method to determine if a string is a +/- sign or not
+     * @param termToCheck string being checked
+     * @return true if string was a + or -
+     */
     private static boolean isSign(String termToCheck) {
 
+        // Make a list of legal signs and return if termToCheck ends in one or not
+        String legalSigns = "+-";
+        if (termToCheck.length() > 0 &&
+                legalSigns.contains(String.valueOf(termToCheck.charAt(termToCheck.length() - 1)))) return true;
 
-        Term tempTerm = new Term();
-
-        if (termToCheck.length() > 1) {
-            if (tempTerm.LEGAL_SIGNS.contains(String.valueOf(termToCheck.charAt(termToCheck.length() - 1))))
-                return true;
-        }
         return false;
+    }
 
+    /**
+     * a method to confirm if a char is valid for use as a term
+     * @param charToCheck char being checked
+     * @return false and exit program if not [a-z] || [A-Z]
+     */
+    private static boolean isValidChar(char charToCheck){
+
+        // Create list of legal chars and check if char is in it
+        String legalChars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if (legalChars.contains(String.valueOf(charToCheck))) return true;
+
+        // Invalid? Print error message and end program.
+        System.out.println("The term is not valid. Please try again.");
+        System.exit(0);
+        return false;
+    }
+
+    /**
+     * method to ensure a term input is valid
+     * @param stringToCheck
+     */
+    private static void ensureValidTerm(String stringToCheck){
+
+        String legalChars = "abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+";
+        for (int i = 0; i < stringToCheck.length()-1; i++){
+            if (stringToCheck.charAt(i) == '^') {
+                //Print error message and end program.
+                System.out.println("The term is not valid. Please try again.");
+                System.exit(0);
+            }
+        }
 
     }
 
